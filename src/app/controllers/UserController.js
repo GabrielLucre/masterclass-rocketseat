7aed6cdfe0b758a894056c335d3f3c1c38f41be1
GabrielLucre <gabriellucrecosta@gmail.com>
2024-10-17 17:12:40 -0300

    Adicionando projeto ao repositório

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,19 @@
+import Queue from '../lib/Queue';
+
+export default {
+    async store(req, res) {
+        const { name, email, password } = req.body;
+
+        const user = {
+            name,
+            email,
+            password
+        };
+
+        await Queue.add('RegistrationMail', { user: { name, email } });
+
+        await Queue.add('UserReport', { user: { name, email } });
+
+        return res.json(user);
+    }
+};
